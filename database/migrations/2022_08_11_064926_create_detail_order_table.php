c51c108e548ea394b56725b280c1da582d49b4d7
Ranidya Putri <ranidyaputri02@gmail.com>
2022-08-11 13:59:29 +0700

    add migration table for detail_order, kategori, and detail_transaksi

---
+++ database/migrations/2022_08_11_064926_create_detail_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id');
+            $table->char('tipe_order',100);
+            $table->integer('qty');
+            $table->float('diskon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_order');
+    }
+};
---
+++ database/migrations/2022_08_11_065109_create_detail_transaksi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaksi_id');
+            $table->integer('qty');
+            $table->float('diskon');
+            $table->float('pajak');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transaksi');
+    }
+};
